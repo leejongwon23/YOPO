d289cd6a14e590f7d92c8de3793a9799c58ffa9b
leejongwon23 <rnepfk@gmail.com>
2026-01-21 18:09:54 +0900

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+    COINS: [
+        { symbol: 'BTCUSDT', name: 'Bitcoin' },
+        { symbol: 'ETHUSDT', name: 'Ethereum' },
+        { symbol: 'SOLUSDT', name: 'Solana' },
+        { symbol: 'XRPUSDT', name: 'Ripple' }
+    ],
+    UPDATE_INTERVAL: 5000,
+    API_BASE: 'https://api.bybit.com/v5/market'
+};
